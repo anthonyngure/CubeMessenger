691711694febc1a04243acd51d0adb01514a09cd
anthonyngure <anthonyngure25@gmail.com>
2018-04-26 14:53:49 +0300

    Appointments ui

--- resources/assets/js/components/Appointments.vue
+++ resources/assets/js/components/Appointments.vue
@@ -8,9 +8,72 @@
                     event-color="green"
                     :events="appointmentDates">
             </v-date-picker>
-            <v-card>
+            <v-container fluid grid-list-md>
                 <connection-manager ref="connectionManager"></connection-manager>
-                <v-data-iterator
+                <v-data-iterator content-tag="v-layout"
+                                 row
+                                 wrap
+                                 :items="appointments"
+                                 :rows-per-page-items="rowsPerPageItems"
+                                 :pagination.sync="pagination">
+                    <span slot="no-data" class="pa-5">
+                            <p>No appointments or meetings found for {{date}}</p>
+                        </span>
+                    <v-flex slot="item"
+                            slot-scope="props"
+                            md6>
+                        <v-card :style="'height: '+cardHeight">
+                            <div :style="'max-height: '+cardHeight" class="scroll-y pa-3">
+                                <h2>{{ props.item.title }}</h2>
+                                <h5>{{ props.item.venue }}</h5>
+                                <v-chip label small color="accent" text-color="white">
+                                    <v-icon left>access_time</v-icon>
+                                    {{props.item.allDay ? 'All day'
+                                    : props.item.startingAt+' -'+props.item.endingAt}}
+                                </v-chip>
+                                <v-divider></v-divider>
+                                <v-subheader>Internal Participants</v-subheader>
+                                <v-list avatar>
+                                    <v-list-tile v-for="participant in props.item.internalParticipants"
+                                                 :key="participant.id">
+                                        <v-list-tile-action>
+                                            <v-icon color="primary">person</v-icon>
+                                        </v-list-tile-action>
+                                        <v-list-tile-content>
+                                            <v-list-tile-title>{{ participant.name }}</v-list-tile-title>
+                                            <v-list-tile-sub-title>{{ participant.email }}</v-list-tile-sub-title>
+                                            <v-list-tile-sub-title>{{participant.phone}}</v-list-tile-sub-title>
+                                        </v-list-tile-content>
+                                    </v-list-tile>
+                                </v-list>
+                                <v-divider></v-divider>
+                                <v-subheader>External Participants</v-subheader>
+                                <v-list avatar>
+                                    <v-list-tile v-for="participant in props.item.externalParticipants"
+                                                 :key="participant.id">
+                                        <v-list-tile-action>
+                                            <v-icon color="primary">person</v-icon>
+                                        </v-list-tile-action>
+                                        <v-list-tile-content>
+                                            <v-list-tile-title>{{ participant.email }}</v-list-tile-title>
+                                            <v-list-tile-sub-title>{{participant.phone}}</v-list-tile-sub-title>
+                                        </v-list-tile-content>
+                                    </v-list-tile>
+                                </v-list>
+                                <v-divider></v-divider>
+                                <v-subheader>Issues/Items</v-subheader>
+                                <template v-for="(issue, index) in props.item.items">
+                                    <p :key="issue.id">{{issue.details}}</p>
+                                    <v-divider v-if="index !== props.item.items.length - 1"
+                                               :key="'div_'+issue.id"></v-divider>
+                                </template>
+                            </div>
+                        </v-card>
+                    </v-flex>
+                </v-data-iterator>
+            </v-container>
+            <v-card>
+                <!--<v-data-iterator
                         content-tag="v-list"
                         :items="appointments"
                         :rows-per-page-items="rowsPerPageItems"
@@ -64,16 +127,9 @@
                         <v-divider :key="'div_'+props.item.id"></v-divider>
                     </template>
 
-                </v-data-iterator>
+                </v-data-iterator>-->
             </v-card>
 
-        </v-flex>
-
-        <v-flex xs12>
-            <!--<add-appointment-full-dialog :appointmentFullDialog.sync="appointmentFullDialog"
-                                         :selectedCell="selectedCell"
-                                         @onClose="onCloseDialog">
-            </add-appointment-full-dialog>-->
         </v-flex>
 
         <add-appointment-dialog :show="addingAppointment"
@@ -99,7 +155,6 @@
 <script>
 
   import Moment from 'moment'
-  import AddAppointmentFullDialog from './AddAppointmentFullDialog'
   import Base from './Base.vue'
 
   import {extendMoment} from 'moment-range'
@@ -112,8 +167,7 @@
     extends: Base,
     components: {
       AddAppointmentDialog,
-      ConnectionManager,
-      AddAppointmentFullDialog
+      ConnectionManager
     },
     name: 'appointments',
     data () {
@@ -156,6 +210,11 @@
         if (addedAppointmentSuccessfully) {
           this.refresh()
         }
+      }
+    },
+    computed: {
+      cardHeight () {
+        return (this.$vuetify.breakpoint.height * 0.75) + 'px'
       }
     },
     mounted () {
